94b24e7b405fc952f27d7cd7d3bc4ad6cf14d9d7
weichuang <weichuang059@163.com>
2023-04-14 16:15:31 +0800

    Squashed commit of the following:
    commit 988044d1a108b3cd3fab3c16236d8e0320e29a71
    Author: weichuang <weichuang059@163.com>
    Date:   Fri Apr 14 16:15:03 2023 +0800
    
        deploy配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://yirancn.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -20,6 +20,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "YiRanCN", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
+  deploymentBranch: "deploy",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -29,7 +31,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "zh-Hans",
-    locales: ["zh"],
+    locales: ["zh-Hans"],
   },
 
   presets: [
