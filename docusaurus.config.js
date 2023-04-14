9f1ca502a9135e4192747df4fa9f51b76686a8fe
weichuang <weichuang059@163.com>
2023-04-14 18:52:01 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,14 +14,14 @@ const config = {
   url: "https://yirancn.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/idoc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "YiRanCN", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+  projectName: "idoc", // Usually your repo name.
   deploymentBranch: "deploy",
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
