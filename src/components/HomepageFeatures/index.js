3996bb6c0fa81427ec9bf150cf96507a35c5cee9
YiRanCN <cniiot@163.com>
2023-06-24 22:37:04 +0800

    1

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,60 +1,48 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-import tuozhanImg from '@site/static/img/tuo-zhan.png';
-import xieyiImg from '@site/static/img/xie-yi.png';
-import anquanImg from '@site/static/img/an-quan.jpg';
-import dbImg from '@site/static/img/db.png';
-import sdkImg from '@site/static/img/sdk.png';
-import guanliImg from '@site/static/img/guan-li.png';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import tuozhanImg from "@site/static/img/tuo-zhan.png";
+import xieyiImg from "@site/static/img/xie-yi.png";
+import anquanImg from "@site/static/img/an-quan.jpg";
+import dbImg from "@site/static/img/db.png";
+import sdkImg from "@site/static/img/sdk.png";
+import guanliImg from "@site/static/img/guan-li.png";
 const FeatureList = [
   {
-    title: '高可扩展性',
+    title: "高可扩展性",
     img: tuozhanImg,
     description: (
-      <>
-        能够支持大规模设备连接和数据处理，并且可以根据业务需求弹性伸缩。
-      </>
+      <>能够支持大规模设备连接和数据处理，并且可以根据业务需求弹性伸缩。</>
     ),
   },
   {
-    title: '安全性高',
+    title: "安全性高",
     img: anquanImg,
-    description: (
-      <>
-        提供多种安全措施，如设备认证、数据加密传输和访问控制等。
-      </>
-    ),
+    description: <>提供多种安全措施，如设备认证、数据加密传输和访问控制等。</>,
   },
   {
-    title: '多协议支持',
+    title: "多协议支持",
     img: xieyiImg,
     description: (
-      <>
-        支持不同类型的通信协议，例如MQTT, CoAP, HTTP等等。
-      </>
+      <>支持不同类型的通信协议，例如MQTT、CoAP、HTTP、Modbus等等。</>
     ),
   },
   {
-    title: '数据存储与分析',
+    title: "数据存储与分析",
     img: dbImg,
     description: (
       <>
-        支持大规模数据存储和分析，对接接入设备，实现对数据的采集、存储、处理和分析，帮助企业更好地了解设备状态和用户行为。
+        支持大规模数据存储和分析，对接接入设备，实现对数据的采集、存储、处理和分析。
       </>
     ),
   },
   {
-    title: '业务应用快速接入',
+    title: "业务应用快速接入",
     img: sdkImg,
-    description: (
-      <>
-        提供API和SDK，方便开发者快速接入并使用平台功能。
-      </>
-    ),
+    description: <>提供API和SDK，方便开发者快速接入并使用平台功能。</>,
   },
   {
-    title: '强大的管理工具',
+    title: "强大的管理工具",
     img: guanliImg,
     description: (
       <>
@@ -66,14 +54,22 @@ const FeatureList = [
 
 function Feature({ img, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         {/* <Svg className={styles.featureSvg} role="img" /> */}
-        <img src={img} className={styles.featureImg} />
+        <img
+          src={img}
+          className={styles.featureImg}
+          style={{
+            borderRadius: "10px",
+            border: "1px solid #efefef",
+            width: "80%",
+          }}
+        />
       </div>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <h3 style={{ fontSize: "18px" }}>{title}</h3>
+        <p style={{ fontSize: "13px" }}>{description}</p>
       </div>
     </div>
   );
